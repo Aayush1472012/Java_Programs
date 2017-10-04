0acf958f7252a022eb9d92b8491633a241f74a12
Kartik mehra <kartikmhr97@gmail.com>
2017-10-04 21:31:14 +0530

    Finding array length using length method

---
+++ array_length.java
@@ -0,0 +1,10 @@
+class array_length {
+  public static void main(String[] args) {
+    int a1[]=new int[10];
+    int a2[]={1,2,3,4,5,6,-10};
+    int a3[]={4,3,2,1};
+    System.out.println("Length of a1 : " + a1.length);
+    System.out.println("Length of a2 : " + a2.length);
+    System.out.println("Length of a3 : " + a3.length);
+  }
+}
