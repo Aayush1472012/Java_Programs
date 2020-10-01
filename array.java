42aa9590f7696034a90d5d7b6e573cb3766361d7
Aayush1472012 <62372257+Aayush1472012@users.noreply.github.com>
2020-10-01 17:54:16 +0530

    Update array.java

--- array.java
+++ array.java
@@ -2,16 +2,10 @@ class array{
   public static void main(String[] args) {
   int arr[];
   arr=new int[10];
-  arr[0]=0;
-  arr[1]=1;
-  arr[2]=2;
-  arr[3]=3;
-  arr[4]=4;
-  arr[5]=5;
-  arr[6]=6;
-  arr[7]=7;
-  arr[8]=8;
-  arr[9]=9;
+  //Assigning values to array
+  for (int i=0; i<10 ; i++){
+            arr[i] = i;
+  }
   System.out.println("arr[3] : " + arr[3]); 
   }
 }
